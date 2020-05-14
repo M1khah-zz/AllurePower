f39e7dd7eed65d2b1e8403f567ecd5c7f0484f30
m1khah <alexeyenko92@gmail.com>
2020-05-14 14:42:41 +0300

    Added  methods toCheckoutPage and DriverActions

--- src/main/java/framework/DriverActions.java
+++ src/main/java/framework/DriverActions.java
@@ -1,11 +1,27 @@
 package framework;
 
 import lombok.extern.log4j.Log4j;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 @Log4j
 public class DriverActions {
 
+    static WebDriver driver = PlatformFactory.getDriver();
+
     public static void open(String url) {
-        PlatformFactory.getDriver().get(url);
+        driver.get(url);
+    }
+
+    public static void scrollDown() {
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("window.scrollBy(0,550)", "");
+    }
+
+    public static void mouseOver(WebElement element){
+        Actions action = new Actions(driver);
+        action.moveToElement(element).perform();
     }
 }
--- src/main/java/pages/CheckoutPage.java
+++ src/main/java/pages/CheckoutPage.java
@@ -1,7 +1,6 @@
 package pages;
 
 
-import com.github.javafaker.Faker;
 import lombok.extern.log4j.Log4j;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,16 +8,8 @@ import org.openqa.selenium.support.FindBy;
 @Log4j
 public class CheckoutPage extends BasePage {
 
-    Faker faker = new Faker();
-
     @FindBy(xpath = "//*[@title=\"Add to cart\" and @data-id-product=\"1\"]")
     public WebElement addToCartButton;
-
-    @FindBy(xpath = "(//a[@title=\"Proceed to checkout\"])[1]")
-    public WebElement proceedToCheckoutButtonModal;
-
-    @FindBy(xpath = "(//a[@title=\"Proceed to checkout\"])[2]")
-    public WebElement proceedToCheckoutButtonPage;
 
     @FindBy(xpath = "//h1[@id = 'cart_title']")
     public WebElement cartTitle;
@@ -35,6 +26,15 @@ public class CheckoutPage extends BasePage {
     @FindBy(xpath = "//button[@name='processCarrier']")
     public WebElement processCarrierButton;
 
+    @FindBy(xpath = "(//a[@title=\"Proceed to checkout\"])[1]")
+    public WebElement proceedToCheckoutButtonModal;
+
+    @FindBy(xpath = "(//a[@title=\"Proceed to checkout\"])[2]")
+    public WebElement proceedToCheckoutButtonPage;
+
+    @FindBy(xpath = "(//a[@title=\"Proceed to checkout\"])[1] or (//a[@title=\"Proceed to checkout\"])[2]) or '//button[@name='processCarrier' or //button[@name='processCarrier']")
+    public WebElement proceedToCheckoutButton;
+
     @FindBy(id = "cgv")
     public WebElement tosCheckbox;
 
@@ -47,5 +47,52 @@ public class CheckoutPage extends BasePage {
     @FindBy(xpath = "//*[text()='Your order on My Store is complete.']")
     public WebElement successSign;
 
+    @FindBy(xpath = "//a[@class='logout']")
+    public WebElement logoutButton;
+
+    public void addItemToCart() {
+        addToCartButton.click();
+    }
+
+    public void clickProceedToCheckout() {
+        proceedToCheckoutButton.click();
+    }
+
+    public String getCartTitle() {
+        return cartTitle.getText();
+    }
+
+    public void increaseProductAmount() {
+        increaseProdictAmountButton.click();
+        log.info("Product amount increased");
+    }
+
+    public String getProductQuantity() {
+        return summaryProductQuantity.getText();
+    }
+
+    public void acceptToS() {
+        tosCheckbox.click();
+        log.info("ToS accepted");
+    }
+
+    public void selectBankwire() {
+        bankwirePaymentButton.click();
+        log.info("Bankwire selected");
+    }
+
+    public void confirmOrder() {
+        confirmOrderButton.click();
+        log.info("Order confirmed");
+    }
+
+    public String checkSuccessSign(){
+        return successSign.getText();
+    }
+
+    public void clickSignOutButton(){
+        logoutButton.click();
+        log.info("Signed out");
+    }
 
 }
